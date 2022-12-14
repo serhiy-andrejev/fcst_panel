441ac21836a4ac7ba4201303ce9d3661d651eead
serhiy-andrejev <120607938+serhiy-andrejev@users.noreply.github.com>
2022-12-15 01:52:38 +0200

    burger button remove

--- forecast_panel.py
+++ forecast_panel.py
@@ -26,12 +26,12 @@ import itertools
 import streamlit as st
 
 st.set_page_config(layout="wide")
-#hide_menu_style = """
-#        <style>
-#        #MainMenu {visibility: hidden;}
-#        </style>
-#        """
-#st.markdown(hide_menu_style, unsafe_allow_html=True)
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 
 @st.cache()
